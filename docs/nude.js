1e74e2f615332b32431a8d7909691ade6ba0eb44
mezoistvan <mezoistvan1@gmail.com>
2017-04-17 23:19:53 +0200

    hello tone

---
+++ docs/nude.js
@@ -0,0 +1,5 @@
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
